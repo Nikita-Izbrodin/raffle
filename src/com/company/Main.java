76608ec813e3e4bd143e0cb3b2fc626d5c07105c
Nikita Izbrodin <90528953+NI210248@users.noreply.github.com>
2021-11-19 15:48:52 +0000

    added input and outputs to buying or checking ruffle tickets (using switch)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,50 @@
 package com.company;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+
         Scanner input = new Scanner(System.in);
+        String userName;
+        //int raffleNum = rand.nextInt(1000);
+        int raffleNum;
+        Random rand = new Random();
+
         System.out.println("Would you like to check a raffle ticket or buy one? c/b");
         String raffleOpt = input.next();
         switch (raffleOpt) {
             case "c":
-                System.out.println("check");
-                break;
-            case "check":
-                System.out.println("check2");
+                //System.out.println("check");
+            
+                System.out.println("What is your name? ");
+                userName = input.next();
+                System.out.println("The name you entered is: " + userName);
+                
+                System.out.println("What is your 3-digit raffle ticket number? ");
+                raffleNum = input.nextInt();
+                System.out.println("The number you entered is: " + raffleNum);
+
                 break;
             case "b":
-                System.out.println("buy");
-                break;
-            case "buy":
-                System.out.println("buy2");
+                //System.out.println("buy");
+
+                System.out.println("What is your name? ");
+                userName = input.next();
+
+                //System.out.println("The name you entered is: " + userName);
+
+                raffleNum = rand.nextInt(1000);
+
+                //System.out.println("The number you got is: " + raffleNum);
+
+                System.out.println("Your raffle ticket is "+ userName + ":" + raffleNum);
                 break;
         }
 
