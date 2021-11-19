08c05506bbd20c473ef6af76ea424a30906ccf83
Nikita Izbrodin <90528953+NI210248@users.noreply.github.com>
2021-11-19 15:48:52 +0000

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,14 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        Random rand = new Random();
+
         System.out.println("Would you like to check a raffle ticket or buy one? c/b");
         String raffleOpt = input.next();
         switch (raffleOpt) {
             case "c":
-                System.out.println("check");
+                //System.out.println("check");
+            
+                System.out.println("What is your name? ");
+                String userName = input.next();
+                
+                System.out.println("What is your 3-digit raffle ticket number? ");
+                int raffleNum = rand.nextInt(1000);
+                System.out.println(raffleNum);
                 break;
+                
             case "check":
-                System.out.println("check2");
+                //System.out.println("check2");
                 break;
             case "b":
                 System.out.println("buy");
