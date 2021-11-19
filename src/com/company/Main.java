776b0ce08a51b1630318c023df891475de6ae21b
Nikita Izbrodin <NI210248@reigate.ac.uk>
2021-11-19 19:29:50 +0000

    made changes to switch (can input name and input/generate raffle number). attempted using BufferedReader.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,49 @@
 package com.company;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        //BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+
         Scanner input = new Scanner(System.in);
+        String userName;
 
         Random rand = new Random();
+        int raffleNum;
 
         System.out.println("Would you like to check a raffle ticket or buy one? c/b");
         String raffleOpt = input.next();
-        switch (raffleOpt) {
-            case "c":
-                //System.out.println("check");
 
+        switch (raffleOpt) {
+
+            case "c":
                 System.out.println("What is your name? ");
-                String userName = input.next();
+                userName = input.next();
 
                 System.out.println("What is your 3-digit raffle ticket number? ");
-                int raffleNum = rand.nextInt(1000);
-                System.out.println(raffleNum);
+                raffleNum = input.nextInt();
+
+                System.out.println("The raffle you entered is " + userName + ":" + raffleNum);
+
                 break;
 
-            case "check":
-                //System.out.println("check2");
-                break;
             case "b":
-                System.out.println("buy");
-                break;
-            case "buy":
-                System.out.println("buy2");
+                System.out.println("What is your name? ");
+                userName = input.next();
+
+                raffleNum = rand.nextInt(1000);
+
+                System.out.println("Your raffle is " + userName + ":" + raffleNum);
+
                 break;
         }
 
     }
 }
-
-// change 2
