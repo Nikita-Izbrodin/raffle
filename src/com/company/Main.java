54e17fc1d7752e44fcad06b0f8ef1bd36e4f08d8
NI210248 <NI210248@reigate.ac.uk>
2021-11-22 12:06:10 +0000

    Added buffered reader. Tried using try and catch however this makes program not work as intended.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,60 @@
 package com.company;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
-
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Random;
+
+
 
 public class Main {
 
-    public static void main(String[] args) {
 
-        //BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
 
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        boolean loop = true;
+
+        BufferedReader input = new BufferedReader (new InputStreamReader(System.in));
         String userName;
 
         Random rand = new Random();
         int raffleNum;
 
         System.out.println("Would you like to check a raffle ticket or buy one? c/b");
-        String raffleOpt = input.next();
+        String raffleOpt = input.readLine();
 
         switch (raffleOpt) {
 
             case "c":
                 System.out.println("What is your name? ");
-                userName = input.next();
+                userName = input.readLine();
 
-                System.out.println("What is your 3-digit raffle ticket number? ");
-                raffleNum = input.nextInt();
+                do {
+                    try {
+                        System.out.println("What is your 3-digit raffle ticket number? ");
+                        raffleNum = Integer.parseInt (input.readLine());
+                        loop = false;
+                        //break;
+                    } catch (Exception e) {
+                        System.out.println("You did not enter a number.");
+                    }
 
-                System.out.println("The raffle you entered is " + userName + ":" + raffleNum);
+                    System.out.println("What is your 3-digit raffle ticket number? ");
+                    raffleNum = Integer.parseInt (input.readLine());
+
+                    System.out.println("The raffle you entered is " + userName + ":" + raffleNum);
+
+                } while (loop);
+
+
+
 
                 break;
 
             case "b":
                 System.out.println("What is your name? ");
-                userName = input.next();
+                userName = input.readLine();
 
                 raffleNum = rand.nextInt(1000);
 
